fc9c5497023a18f8dcce39c4587fa60bc7d916e9
acgrigores <acgrigores@gmail.com>
2024-05-22 15:12:51 +0000

    now displays database using pandas

---
+++ readdb.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+import pandas as pd
+
+# Path to your modified SQL file
+sql_file_path = "Example-Data.db"
+
+# Connect to (or create) the SQLite database
+conn = sqlite3.Connection(database=sql_file_path)
+df = pd.read_sql_query("SELECT * FROM cloc", con=conn)
+conn.close()
+
+print(df)
