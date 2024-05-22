dd68edf3865e4e90a2fcf784c0c1710acf27fbf8
acgrigores <acgrigores@gmail.com>
2024-05-22 15:57:13 +0000

    did matplotlib graph

---
+++ pythonFiles/matplotlibGraph.py
@@ -0,0 +1,18 @@
+# importing stuff
+import sqlite3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Path to .db file
+sql_file_path = "../Example-Data.db"
+
+# Connect to the SQLite database
+conn = sqlite3.Connection(database=sql_file_path)
+df = pd.read_sql_query("SELECT * FROM cloc LIMIT 10", con=conn)
+conn.close()
+
+print(df["file_count"][0:10])
+plt.locator_params(axis="x", integer=True)
+df.plot.line(x="index", y="file_count", color="red")
+plt.savefig("test.png")
--- pythonFiles/readWithPandas.py
+++ pythonFiles/readWithPandas.py
@@ -1,11 +1,12 @@
+# importing stuff
 import sqlite3
 
 import pandas as pd
 
-# Path to your modified SQL file
+# Path to .db file
 sql_file_path = "../Example-Data.db"
 
-# Connect to (or create) the SQLite database
+# Connect to the SQLite database
 conn = sqlite3.Connection(database=sql_file_path)
 df = pd.read_sql_query("SELECT * FROM cloc", con=conn)
 conn.close()
