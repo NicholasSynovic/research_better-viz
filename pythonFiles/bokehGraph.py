c9605132b9190490d37c92317a0b3cc6745e6fa6
acgrigores <acgrigores@gmail.com>
2024-05-22 18:50:25 +0000

    alllmost made bokeh work

--- pythonFiles/bokehGraph.py
+++ pythonFiles/bokehGraph.py
@@ -2,18 +2,23 @@
 import sqlite3
 
 import pandas as pd
-from bokeh.plotting import figure, show
+from bokeh.io import export_png, output_notebook
+from bokeh.plotting import figure, save, show
 
 # Path to .db file
 sql_file_path = "../Example-Data.db"
 
 # Connect to the SQLite database
 conn = sqlite3.Connection(database=sql_file_path)
+# dfx = pd.read_sql_query("SELECT index FROM cloc LIMIT 10", con=conn)
+# dfy = pd.read_sql_query("SELECT file_count FROM cloc LIMIT 10", con=conn)
 df = pd.read_sql_query("SELECT * FROM cloc LIMIT 10", con=conn)
 conn.close()
 
-p = figure()
-p.line(x=df["index"], y=df["file_count"], legend_label="Temp.", line_width=2)
+x = df.index.to_list()
+y = df["file_count"].to_list()
+
 print(df)
-output_file("line.html")
-show(p)
+p = figure()
+p.line(x=x, y=y, legend_label="Temp.", line_width=2)
+export_png(p, filename="test.png")
