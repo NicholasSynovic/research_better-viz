c9580020e4f6cb00251deed088e9afd66ab1a1f1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-29 13:36:12 -0500

    Add type hint

--- src/test_plotly.py
+++ src/test_plotly.py
@@ -1,11 +1,12 @@
 import plotly.express as px
 import plotly.io as pio
+from plotly.graph_objects import Figure
 
 from src import DF
 
 
 def main() -> None:
-    fig = px.line(DF, x="x", y="y", title="Example Chart")
+    fig: Figure = px.line(DF, x="x", y="y", title="Example Chart")
     pio.write_image(fig, "img/plotly.png", width=800, height=600)
 
 
