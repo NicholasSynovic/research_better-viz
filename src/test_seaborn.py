8cb8d737dbf457944cbbc02a530675a9b2fd6934
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-29 13:42:15 -0500

    Add seaborn refactored code and delete images

--- src/test_seaborn.py
+++ src/test_seaborn.py
@@ -1,5 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from src import DF
+
+
 def main() -> None:
-    pass
+    sns.relplot(kind="line", x="x", y="y", data=DF)
+    plt.ylabel("Y Axis Label")
+    plt.xlabel("X Axis Label")
+    plt.title("Example Chart")
+    plt.tight_layout()
+    plt.savefig("img/seaborn.png")
 
 
 if __name__ == "__main__":
